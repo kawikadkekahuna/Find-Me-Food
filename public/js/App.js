1a17d5a58d47b4a8e08002e68338e0a887f9455b
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-08-02 22:15:07 +0000

    added ng-click to route to /maps

--- public/js/App.js
+++ public/js/App.js
@@ -8,6 +8,9 @@ angular.module('App', ['ngRoute'])
 			.when('/map',{
 				templateUrl: 'views/map.html',
 				controller: 'MapController'
+			})
+			.otherwise({
+				templateUrl: 'views/404.html'
 			});
 
 	}]);
