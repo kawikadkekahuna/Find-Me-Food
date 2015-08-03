6f94b0b1e8222fc2f71969f024996a07761fe7f8
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-08-03 13:59:44 +0000

    added passport and registration control initial file

---
+++ public/js/controllers/registration-controller.js
@@ -0,0 +1,4 @@
+angular.module('App')
+	.controller('RegistrationController', ['$scope', function($scope){
+		$scope.test = 'hello';
+	}])
--- server.js
+++ server.js
@@ -1,6 +1,41 @@
 var express = require('express');
 var app = express();
+var session = require('express-session');
+var passport = require('./middleware/passport-middleware');
+var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(session({
+	secret: 'plsfindmyfood',
+	resave: false,
+	saveUninitialized: true
+}));
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 
+
+
+app.route('/login') 
+	.post(function(req, res, next) {
+		passport.authenticate('local', function(err, user, info) {
+			console.log('user', user);
+			if (err) {
+				return next(err);
+			}
+
+			if (!user) {
+				return res.send(false);
+			}
+			req.logIn(user, function(err) {
+				if (err) {
+					return next(err);
+				}
+				res.status(200).send(true);
+
+			});
+		})(req, res, next);
+	});
 app.listen(3000);
