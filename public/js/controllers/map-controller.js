923e6a205666bef8634465ae6d49b530668276cf
jasontsukamoto <jasontsukamoto@gmail.com>
2015-08-05 11:07:33 -1000

    adds function to find next restaurant

--- public/js/controllers/map-controller.js
+++ public/js/controllers/map-controller.js
@@ -3,6 +3,12 @@
 angular.module('App')
 	.controller('MapController', ['$scope','MapService',function($scope, MapService){
 		MapService.init(angular.element('#map-canvas').get(0), angular.element('#directionsPanel').get(0));
-
-
+    MapService.loaded(function(){
+      $scope.$apply(function() {
+        $scope.restaurantName = "How about " + MapService.getLocationName() + "?";
+      });
+    });
+    $scope.nextRestaurant = function(){
+      MapService.getNextRestaurant(angular.element('#map-canvas').get(0), angular.element('#directionsPanel').get(0));
+    }
 	}]);
